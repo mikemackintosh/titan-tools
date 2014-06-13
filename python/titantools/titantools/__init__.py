31e70dfd66d7097530acd65346be832b83ba23ba
Mike Mackintosh <m@splug.io>
2014-06-13 13:04:50 -0400

    adding intial python package

---
+++ python/titantools/titantools/__init__.py
@@ -0,0 +1,9 @@
+from __future__ import unicode_literals
+
+__copyright__ = "2014, Mike Mackintosh"
+__version__ = '0.0.4'
+
+#try:
+#      __import__('pkg_resources').declare_namespace(__name__)
+#except ImportError:
+#      __path__ = __import__('pkgutil').extend_path(__path__, __name__)
