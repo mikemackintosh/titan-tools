c0996909c403dc13dbd1aa9d802dde1e5e3982cb
Mike Mackintosh <m@splug.io>
2014-06-19 13:02:30 -0400

    adding .gitignore and updating version

--- python/titantools/__init__.py
+++ python/titantools/__init__.py
@@ -1,9 +1,4 @@
 from __future__ import unicode_literals
 
 __copyright__ = "2014, Mike Mackintosh"
-__version__ = '0.0.5'
-
-#try:
-#      __import__('pkg_resources').declare_namespace(__name__)
-#except ImportError:
-#      __path__ = __import__('pkgutil').extend_path(__path__, __name__)
+__version__ = '0.0.9'
